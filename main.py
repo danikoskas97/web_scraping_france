cd4ecfa46f0452c29a1c59f916ce2ce6b2c5ebba
dani koskas <koskas.dani@gmail.com>
2020-05-12 13:39:45 +0200

    not finish

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import time
 
 
 url_2 = f'https://www.giropharm.fr/trouver-ma-pharmacie/resultats/resultat/dpt-75-paris/9.html?giropharm=0'
-
+# you can see that the url 2 have '/9' => one page contains 9 pharma info i need to find a way to do a for loop to get
+# all the all_page for all the pages
 url = 'https://www.giropharm.fr/trouver-ma-pharmacie/resultats/resultat/dpt-75-paris.html?giropharm=0'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 bloc = soup.find(class_='pharmacie_bloc_resultat contenu_vert_clair')
 
-bloc_text = bloc.get_text()
+all_page = bloc.get_text()
 
 print(len(url))
 
